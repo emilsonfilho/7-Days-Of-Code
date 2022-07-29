cb200cc6ca3e8ce73727018b5aceada093dbe82c
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2022-07-29 10:54:00 -0300

    update app.js

--- challenge-1/app.js
+++ challenge-1/app.js
@@ -10,3 +10,15 @@ if (numeroUm == stringUm) {
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
+
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
