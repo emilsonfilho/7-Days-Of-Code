4c0384c949c0a60178eadc8b35c17b9f9dd44b4f
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2022-07-31 14:24:40 -0300

    update optional.js

--- challenge-2/optional.js
+++ challenge-2/optional.js
@@ -1,1 +1,26 @@
+import { linguagem } from './app.js'
+import { body } from './app.js'
 
+let gostando = prompt(`Você está gostando de estudar ${linguagem}?\nDigite 1 para SIM\nDigite 2 para NÃO`)
+
+while (gostando !== "1" && gostando !== "2") {
+  gostando = prompt(`Número inválido.\nVocê está gostando de estudar ${linguagem}?\nDigite 1 para SIM\nDigite 2 para NÃO`)
+}
+
+switch (gostando) {
+  case "1":
+    createDiv(`Que bom! Continue estudando e você terá muito sucesso!`)
+    break;
+  case "2":
+    createDiv(`Ahh... que pena. Já tentou aprender outras linguagens?`)
+  default:
+}
+
+function createDiv(msg) {
+  const div = document.createElement('div')
+  const para = document.createElement('p')
+  para.innerText = msg
+  div.appendChild(para)
+  div.setAttribute('class', 'box')
+  body.appendChild(div)
+}
